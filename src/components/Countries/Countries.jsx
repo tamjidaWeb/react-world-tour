a5ff87eef3b12c821447d16ee4a3c9594d9623a5
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-15 01:54:29 +0600

    commited

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,30 +1,30 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/country";
+import "./Countries.css";
 // import Country from "../Country/country";
 
-
-
 const Countries = () => {
-    const [countries, setCountries] = useState([]);
-    useEffect(()=>{
-       
-        fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountries(data))
-    }, [])
-    
-    
-    
-    return (
-        <div>
-            <h3>Countries :{countries.length}</h3>
-          {
-            countries.map(country => <Country key={country.cca3} country = {country}></Country>)
-            // countries.map(country => <Country key={country.ccs3} country={country}></Country>)
-          }
-        </div>
-    );
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+
+  return (
+    <div>
+      <h3>Countries :{countries.length}</h3>
+      <div className="country-container">
+        {
+          countries.map((country) => (
+            <Country key={country.cca3} country={country}></Country>
+          ))
+          // countries.map(country => <Country key={country.ccs3} country={country}></Country>)
+        }
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
--- src/components/Country/country.jsx
+++ src/components/Country/country.jsx
@@ -1,12 +1,13 @@
 import './Country.css'
 const Country = ({country}) => {
-    const {name, flags, population, area} = country;
+    const {name, flags, population, area, cca3} = country;
     return (
         <div className="country">
             <h3>Name: {name.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
+            <p><small>Code: {cca3}</small></p>
         </div>
     );
 };
