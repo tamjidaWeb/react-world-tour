174df57ec3658bac01c8062ff4470ae96793d8cf
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-14 23:58:58 +0600

    problem solved

--- src/components/Country/country.jsx
+++ src/components/Country/country.jsx
@@ -1,9 +1,10 @@
 
 const Country = ({country}) => {
+    console.log(country);
     const {name} = country;
     return (
         <div>
-            <h3>Name: {name.common}</h3>
+            <h3>Name: {name?.common}</h3>
         </div>
     );
 };
