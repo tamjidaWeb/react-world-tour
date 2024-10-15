c91f98a7b0b8110fe5d5a754a8b1296e1c4437ea
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-15 14:00:12 +0600

    commited

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -16,16 +16,19 @@ const Countries = () => {
 
 const handleVisitedCountry = country =>{
   console.log('add this to your visited country');
-  // console.log(country);
+const newVisitedCountries = [...visitedCountries, country];
+setVisitedCountries(newVisitedCountries);
 }
 
   return (
     <div>
       <h3>Countries :{countries.length}</h3>
       <div>
-      <h5>Visited Countries</h5>
+      <h5>Visited Countries: {visitedCountries.length}</h5>
     <ul>
-      li
+      {
+        visitedCountries.map(country=> <li key={country.cca3}>{country.name.common}</li>)
+      }
     </ul>
       </div>
       <div className="country-container">
